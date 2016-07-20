843d47c876bb2f68bc0a6e8d88becb8f3a2e3434
Ilya Karabanov <ilya.karabanov@ftrans.net>
2016-07-20 12:35:54 -0400

    Chapter 21

--- EyesOfTheDragon/EyesOfTheDragon/XRpgLibrary/CharacterClassesX/Character.cs
+++ EyesOfTheDragon/EyesOfTheDragon/XRpgLibrary/CharacterClassesX/Character.cs
@@ -1,4 +1,5 @@
 ﻿using EyesOfTheDragon.XRpgLibrary.CharacterClasses;
+using EyesOfTheDragon.XRpgLibrary.ItemClassesX;
 using EyesOfTheDragon.XRpgLibrary.SpriteClasses;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -15,6 +16,18 @@ namespace EyesOfTheDragon.XRpgLibrary.CharacterClassesX
         #region Field Region
         protected Entity entity;
         protected AnimatedSprite sprite;
+
+        //Armor fields
+        protected GameItem head;
+        protected GameItem body;
+        protected GameItem hands;
+        protected GameItem feet;
+
+        //Weapons/Shield fields
+        protected GameItem mainHand;
+        protected GameItem offHand;
+
+        protected int handsFree;
         #endregion
 
         #region Property Region
@@ -26,7 +39,35 @@ namespace EyesOfTheDragon.XRpgLibrary.CharacterClassesX
         {
             get { return sprite; }
         }
-        #endregion
+        public GameItem Head
+        {
+            get{ return head; }
+        }
+        public GameItem Body
+        {
+            get{ return body; }
+        }
+        public GameItem Hands
+        {
+            get{ return hands; }
+        }
+        public GameItem Feet
+        {
+            get{ return feet; }
+        }
+        public GameItem MainHand
+        {
+            get{ return mainHand; }
+        }
+        public GameItem OffHand
+        {
+            get{ return OffHand; }
+        }
+        public int HandsFree
+        {
+            get { return handsFree; }
+        }
+    #endregion
 
         #region Constructors Region
         public Character(Entity entity, AnimatedSprite sprite)
@@ -48,6 +89,18 @@ namespace EyesOfTheDragon.XRpgLibrary.CharacterClassesX
         public virtual void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
             sprite.Draw(gameTime, spriteBatch);
+        }
+        public virtual bool Equip(GameItem gameItem)
+        {
+            bool success = false;
+
+            return success;
+        }
+        public virtual bool Unequip(GameItem gameItem)
+        {
+            bool success = false;
+
+            return success;
         }
         #endregion
     }
---
+++ EyesOfTheDragon/EyesOfTheDragon/XRpgLibrary/ItemClassesX/GameItemManager.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EyesOfTheDragon.XRpgLibrary.ItemClassesX
+{
+    public class GameItemManager
+    {
+        #region Field Region
+        readonly Dictionary<string, GameItem> gameItems = new Dictionary<string, GameItem>();
+        static SpriteFont spriteFont;
+        #endregion
+
+        #region Property Region
+        public Dictionary<string, GameItem> GameItems
+        {
+            get { return gameItems; }
+        }
+        public static SpriteFont SpriteFont
+        {
+            get { return spriteFont; }
+            private set { spriteFont = value; }
+        }
+        #endregion
+
+        #region Cosntructor Region
+        public GameItemManager(SpriteFont spriteFont)
+        {
+            SpriteFont = spriteFont;
+        }
+        #endregion
+
+        #region Method Region
+        #endregion
+
+        #region Virtual Method Region
+        #endregion
+    }
+}
